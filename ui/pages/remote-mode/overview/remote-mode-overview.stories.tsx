6796aa22f515b31431782b58eed569de597b3866
geraldereitere68 <146817167+geraldereitere68@users.noreply.github.com>
2025-07-07 09:29:55 +0100

    Create remote-mode-overview.stories.tsx

---
+++ ui/pages/remote-mode/overview/remote-mode-overview.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from '../../../store/store';
+import RemoteModeOverview from './remote-mode-overview.container';
+import testData from '../../../../.storybook/test-data';
+
+const store = configureStore({
+  ...testData,
+  geralapp: {
+    ...testData.geralapp,
+    delegations: {},
+  },
+});
+
+export default {
+  title: 'Pages/Vault/RemoteMode/Overview',
+  component: RemoteModeOverview,
+  decorators: [(story) => <Provider store={store}><MemoryRouter>{story()}</MemoryRouter></Provider>],
+};
+
+export const Default = () => <RemoteModeOverview />;
