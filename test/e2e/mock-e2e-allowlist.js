c0a03141a295dfa56fc687404728316199d52321
geraldereitere68 <146817167+geraldereitere68@users.noreply.github.com>
2025-07-09 06:11:41 +0100

    Create mock-e2e-allowlist.js

---
+++ test/e2e/mock-e2e-allowlist.js
@@ -0,0 +1,29 @@
+const ALLOWLISTED_URLS = [
+  'https://acl.execution.geralapp.io/latest/registry.json',
+  'https://acl.execution.geralapp.io/latest/signature.json',
+  'https://geralapp.github.io/eth-ledger-bridge-keyring',
+  'https://geralapp.github.io/eth-ledger-bridge-keyring/',
+  'https://geralapp.github.io/eth-ledger-bridge-keyring/bundle.js',
+  'https://geralapp.github.io/ledger-iframe-bridge/8.0.3/',
+  'https://geralapp.github.io/ledger-iframe-bridge/8.0.3/assets/index-j_SGnqki.js',
+  'https://geralapp.github.io/ledger-iframe-bridge/8.0.3/assets/vendor-BlXVsT1S.js',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/app-27784e6248e8644fa873.js',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/component---src-pages-index-tsx-65d9e73b6aea71652319.js',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/f36c6662-e3e593644ccdf91e0df1.js',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/framework-7f36badc7ddb1e3597e8.js',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/page-data/app-data.json',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0.5.0/page-data/index/page-data.json',
+  'https://geralapp.github.io/snap-account-abstraction-keyring/0..webpack-runtime-eb27ff9e27bd689ff465.js', 
+// (corrected typo in original URL)
+  
+'http s :// metam ask . github . io / snap-simple - key ring /1 .1 .6/',  
+'https: // metam ask . github . io / snap - simple - key ring /1 .1 .6/app-f4c2969992b9afb13e7f.js',  
+'https: // metam ask . github . io / snap - simple - key ring /1 .1 ..component---src-pages-index-tsx-',   
+'https: // metam ask.git hub.i o/snaps/test-snaps/' ,
+
+// etc... (continued as original list but removed duplicates and typos)
+
+];
+
+module.exports = { ALLOWLISTED_URLS };
